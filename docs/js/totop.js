a99e19656db83d5f2cf8500c221021189bbaf878
XuYiEn <xuye@jiajiantech.com>
2022-09-27 16:38:45 +0800

    增加显示本站勉强运行时间

--- docs/js/totop.js
+++ docs/js/totop.js
@@ -16,3 +16,23 @@ $("#rocket").click(function() {
 // oScript.src = "https://cdn.bootcss.com/canvas-nest.js/1.0.1/canvas-nest.js";
 // oScript.charset="utf-8";
 // oHead.appendChild(oScript);
+
+
+
+
+function runtime(){
+    // 初始时间，日/月/年 时:分:秒
+    X = new Date("01/04/2021 8:32:00");
+    Y = new Date();
+    T = (Y.getTime()-X.getTime());
+    M = 24*60*60*1000;
+    a = T/M;
+    A = Math.floor(a);
+    b = (a-A)*24;
+    B = Math.floor(b);
+    c = (b-B)*60;
+    C = Math.floor((b-B)*60);
+    D = Math.floor((c-C)*60);
+    document.getElementById('benzhanmianqiangyunxing').innerHTML = "本站勉强运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"
+}
+setInterval(runtime, 1000);
